073c70e6806e62cc856f932728ae11359f58c33e
Katarzyna <kasiakadziolka@gmail.com>
2022-05-11 17:08:54 +0200

    <3

---
+++ cthulhu-wizard-app/src/features/wizard/atoms/Input.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const value = ref(null);
+const props = defineProps({
+  watermark: {
+    type: String,
+    default: "",
+  },
+  hasTooltip: {
+    type: Boolean,
+    default: false,
+  },
+  tooltipText: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <QInput v-model="value" filled :label="watermark">
+    <QTooltip
+      v-if="hasTooltip"
+      anchor="center right"
+      self="center left"
+      :offset="[10, 10]"
+    >
+      {{ tooltipText }}
+    </QTooltip>
+  </QInput>
+</template>
