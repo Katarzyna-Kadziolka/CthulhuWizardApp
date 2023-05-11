7fada3df9e599c0f37ce2301b70bc8c1337ac85f
Katarzyna <kasiakadziolka@gmail.com>
2023-05-11 15:12:22 +0200

    init

---
+++ CthulhuWizardAppTests/CthulhuWizardAppTests/Drivers/Driver.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace CthulhuWizardAppTests.Drivers {
+    public class Driver {
+    }
+}
---
+++ CthulhuWizardAppTests/CthulhuWizardAppTests/Hooks/Hook.cs
@@ -0,0 +1,8 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace CthulhuWizardAppTests.Hooks {
+    [Binding]
+    public class Hooks {
+    }
+}
